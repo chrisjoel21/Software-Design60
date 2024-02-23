cc5bb6dbfde10554745038b4508946922f1669c7
muhammedIftikhar <omerifti@hotmail.com>
2024-02-23 13:32:04 -0800

    Adding quote history

--- src/components/QuoteHistory/QuoteHistory.js
+++ src/components/QuoteHistory/QuoteHistory.js
@@ -20,8 +20,31 @@ class QuoteHistory extends React.Component {
        <div class='containerQuote center'>
        <body>
           <h1>Quote History</h1>
-          
+          <table>
+            <thead>
+              <tr>
+                <th>Client Name</th>
+                <th>Gallons Requested</th>
+                <th>Delivery Address</th>
+                <th>Delivery Date</th>
+                <th>Price/Gallon</th>
+                <th>Total Amount Due</th>
+              </tr>
+            </thead>
       
+            <tbody>
+              <tr>
+                <td>XYZ</td>
+                <td>5</td>
+                <td>Elgin Street</td>
+                <td>May 10</td>
+                <td>N/A</td>
+                <td>200$</td>
+              </tr>
+      
+            </tbody>
+      
+          </table>
         </body>
        </div>
     
