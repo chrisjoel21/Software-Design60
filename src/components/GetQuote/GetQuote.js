c98dedeed62100910e168b61d5e0b1e4fffcd333
Bluster9110 <81180212+Bluster9110@users.noreply.github.com>
2024-02-23 13:42:13 -0600

    Update GetQuote.js

--- src/components/GetQuote/GetQuote.js
+++ src/components/GetQuote/GetQuote.js
@@ -1,3 +1,4 @@
+/*GetQuote.js*/
 import React from 'react';
 
 class GetQuote extends React.Component {
@@ -27,7 +28,60 @@ class GetQuote extends React.Component {
     return (
       <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
         <div className="">
-          <h1>GET A QUOTE</h1>
+          <h1>GET A NEW QUOTE</h1>
+          <form onSubmit={this.onSubmitSignIn}>
+            <label htmlFor="gallonsRequested">Gallons Requested:</label>
+            <input
+              type="number"
+              id="gallonsRequested"
+              name="gallonsRequested"
+              value={gallonsRequested}
+              onChange={this.handleInputChange}
+              required
+            />
+
+            <div>
+              <label>Delivery Address:</label>
+              {/* Display client profile delivery address here */}
+              <p>Client Profile Delivery Address</p>
+            </div>
+
+            <div>
+              <label>Delivery Date:</label>
+              <input
+                type="date"
+                id="deliveryDate"
+                name="deliveryDate"
+                value={deliveryDate}
+                onChange={this.handleInputChange}
+                required
+              />
+            </div>
+
+            <div>
+              <label>Suggested Price / Gallon:</label>
+              <input
+                type="number"
+                id="suggestedPrice"
+                name="suggestedPrice"
+               // value={/* Add logic to calculate suggested price */}
+                readOnly
+              />
+            </div>
+
+            <div>
+              <label>Total Amount Due:</label>
+              <input
+                type="number"
+                id="totalAmountDue"
+                name="totalAmountDue"
+              //  value={/* Add logic to calculate total amount due */}
+                readOnly
+              />
+            </div>
+
+            <button type="submit">Submit</button>
+          </form>
         </div>
       </article>
     );
